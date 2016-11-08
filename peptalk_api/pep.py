eaecf3d6f15372a2909ec2d9c1c915dec9818b23
Martin Leandertz <martinleandertz@Martins-MacBook-Pro.local>
2016-11-09 00:29:03 +0100

    Stubbar till REST metoder för peps klara

--- peptalk_api/pep.py
+++ peptalk_api/pep.py
@@ -1,13 +1,32 @@
 #Importerar Flask klassen och Request klassen till koden
-from flask import Flask, Request
+from flask import Flask, request
 
 # instansierar ett objekt av klassen Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def index():
-    return "This is a test"
+    return "Testing root path. If you see this it works for %s -requests" % request.method
 
-Export FLASK_APP=pep.py
-$ app.run 
-    * run on http://127.0.0.1/pep.py 
+@app.route("/v1/peps/", methods=['GET'])
+def AllPeps():
+    return "Read a file with all peps and return them as JSON"
+
+@app.route("/v1/peps/<int:pep_id>", methods=['GET'])
+def OnePep(pep_id):
+    return "Read a file and get the pep with id %s" % pep_id
+
+@app.route("/v1/peps/", methods=['POST'])
+def AddPep():
+    return "Adds a pep from request input data"
+
+@app.route("/v1/peps/<int:pep_id>", methods=['PUT'])
+def UpdatePep(pep_id):
+    return "Update the pep with id = %s with the text: %s" % pep_id, % request.data
+
+@app.route("/v1/peps/<int:pep_id>", methods=['DELETE'])
+def RemovePep(pep_id):
+    return "Remove the pep from file where id = %s" % pep_id
+
+if __name__ == "__main__":
+    app.run(debug=True)
