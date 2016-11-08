01bb1a6320b2a87650d5748d15afb84fff346467
Martin Leandertz <martinleandertz@Martins-MacBook-Pro.local>
2016-11-08 23:05:05 +0100

    Commit från Visual Studio Code

--- peptalk_api/pep.py
+++ peptalk_api/pep.py
@@ -1,1 +1,13 @@
-#this is the placeholder for api folder in git
+#Importerar Flask klassen och Request klassen till koden
+from flask import Flask, Request
+
+# instansierar ett objekt av klassen Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "This is a test"
+
+Export FLASK_APP=pep.py
+$ app.run 
+    * run on http://127.0.0.1/pep.py 
